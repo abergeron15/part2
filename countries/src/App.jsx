d6bccccb23d708985d69a3b091eeab918b7ac558
Barthur B <alexbergeron15@gmail.com>
2025-02-11 15:38:42 -0500

    exercise 2.19

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -3,27 +3,30 @@ import countriesServices from './services/countries'
 
 import './index.css'
 
-const Country = ({ country }) =>
-(
-  <div>
-    <h1>{country.name.common}</h1>
-    <div>
-      capital {country.capital}
-    </div>
-    <div>
-      area {country.area}
-    </div>
-    <h3>languages:</h3>
-    <ul>
-      {Object.keys(country.languages).map(l => <li key={l}>{country.languages[l]}</li>)}
-    </ul>
-    <div>
-      <img height='150' src={country.flags.svg} alt={country.flags.alt} />
-    </div>
-  </div>
-)
+const Country = ({ country }) => {
+  return country === null
+    ? null
+    : (
+      <div>
+        <h1>{country.name.common}</h1>
+        <div>
+          capital {country.capital}
+        </div>
+        <div>
+          area {country.area}
+        </div>
+        <h3>languages:</h3>
+        <ul>
+          {Object.keys(country.languages).map(l => <li key={l}>{country.languages[l]}</li>)}
+        </ul>
+        <div>
+          <img height='150' src={country.flags.svg} alt={country.flags.alt} />
+        </div>
+      </div>
+    )
+}
 
-const CountryDisplay = ({ countries }) => {
+const CountryList = ({ countries, showCountry }) => {
   if (countries.length >= 250) {
     return (
       <div></div>
@@ -36,16 +39,13 @@ const CountryDisplay = ({ countries }) => {
   }
   else if (countries.length !== 1) {
     return (
-      <div>
-        {countries.map(c =>
-          <div key={c.name.common}>{c.name.common}</div>
-        )}
-      </div>
-    )
-  }
-  else {
-    return (
-      <Country country={countries[0]} />
+      <table>
+        <tbody>
+          {countries.map(c =>
+            <tr key={c.name.common}><td>{c.name.common}</td><td><button onClick={(event) => showCountry(event, c.name.common)}>show</button></td></tr>
+          )}
+        </tbody>
+      </table>
     )
   }
 }
@@ -55,22 +55,34 @@ const App = () => {
   const [search, setSearch] = useState('')
   const [country, setCountry] = useState(null)
 
-  console.log(matches)
+  console.log('matches', matches)
+  console.log('country', country)
 
   useEffect(() => {
     console.log('effect, getting all countries')
     countriesServices
       .getAll()
       .then(allCountries => {
-        setMatches(allCountries.filter(c => search === '' ? true : c.name.common.toLowerCase().includes(search) || c.name.official.toLowerCase().includes(search)))
+        const matchedCountries = allCountries.filter(c => search === '' ? true : c.name.common.toLowerCase().includes(search) || c.name.official.toLowerCase().includes(search))
+        setMatches(matchedCountries)
+        if (matchedCountries.length === 1) {
+          setCountry(matchedCountries[0])
+        } else {
+          setCountry(null)
+        }
       })
   }, [search])
 
-
-
   const handleSearchChange = (event) => {
     console.log(event.target)
-    setSearch(event.target.value)
+    setSearch(event.target.value.toLowerCase())
+  }
+
+  const handleShowCountry = (event, name) => {
+    console.log(event)
+    console.log('name', name)
+    const selectedCountry = matches.find(c => c.name.common.toLowerCase() === name.toLowerCase())
+    setCountry(selectedCountry)
   }
 
   return (
@@ -78,7 +90,8 @@ const App = () => {
       <div>
         find countries:
         <input value={search} onChange={handleSearchChange} />
-        <CountryDisplay countries={matches} />
+        <CountryList countries={matches} showCountry={handleShowCountry} />
+        <Country country={country} />
       </div>
     </div>
   )
