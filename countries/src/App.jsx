9dfdab6025d0d1e84401bb3ca37b68971422b429
Barthur B <alexbergeron15@gmail.com>
2025-02-11 16:49:38 -0500

    exercise 2.20

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -3,6 +3,19 @@ import countriesServices from './services/countries'
 
 import './index.css'
 
+const Weather = ({ country, weather }) => {
+  return weather === null
+    ? null
+    : (
+      <div>
+        <h2>Weather in {country.capital}</h2>
+        <div>temperature {(weather.current.temp - 273.15).toFixed(2)} Celcius</div>
+        <div>{weather.current.weather.map(w => <img key={w.description} src={`https://openweathermap.org/img/wn/${w.icon}@2x.png`} />)}</div>
+        <div>wind {weather.current.wind_speed} m/s</div>
+      </div>
+    )
+}
+
 const Country = ({ country }) => {
   return country === null
     ? null
@@ -54,6 +67,7 @@ const App = () => {
   const [matches, setMatches] = useState([])
   const [search, setSearch] = useState('')
   const [country, setCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
 
   console.log('matches', matches)
   console.log('country', country)
@@ -69,9 +83,27 @@ const App = () => {
           setCountry(matchedCountries[0])
         } else {
           setCountry(null)
+          setWeather(null)
         }
       })
+      .catch(error => {
+        console.log(error)
+      })
   }, [search])
+
+  useEffect(() => {
+    if (country !== null) {
+      countriesServices
+        .weather(country)
+        .then(returnedWeather => {
+          console.log(returnedWeather)
+          setWeather(returnedWeather)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }, [country])
 
   const handleSearchChange = (event) => {
     console.log(event.target)
@@ -92,6 +124,7 @@ const App = () => {
         <input value={search} onChange={handleSearchChange} />
         <CountryList countries={matches} showCountry={handleShowCountry} />
         <Country country={country} />
+        <Weather country={country} weather={weather} />
       </div>
     </div>
   )
--- countries/src/services/countries.js
+++ countries/src/services/countries.js
@@ -1,5 +1,11 @@
 import axios from "axios";
+
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/";
+
+console.log(import.meta.env);
+
+const openWeatherAPIkey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+const openWeatherURL = `https://api.openweathermap.org/data/3.0/onecall?&appid=${openWeatherAPIkey}`;
 
 const getAll = () => {
   const request = axios.get(baseUrl + "all");
@@ -21,4 +27,24 @@ const del = (id) => {
   return request.then((response) => response.data);
 };
 
-export default { getAll, create, update, del };
+const weather = (country) => {
+  console.log(country);
+  let [lat, lon] = country.latlng;
+
+  if (Object.keys(country.capitalInfo).length) {
+    console.log(country.capitalInfo);
+    [lat, lon] = country.capitalInfo.latlng;
+  }
+
+  console.log([lat, lon]);
+  console.log("lat: " + lat + ", lon: " + lon);
+  const request = axios.get(openWeatherURL, {
+    params: {
+      lat,
+      lon,
+    },
+  });
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, update, del, weather };
